626f33dc391453495265690f1c9dffb35a45bcbb
tkachvd <93051155+tkachvd@users.noreply.github.com>
2022-03-16 21:09:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -5,40 +5,50 @@ int main()
 {
     FILE *input, *output;
     char a, s[30][30];
+    int s2[30][30];
     int i = 0, j = 0, m = 0, n = 0, count = 0, b = 0;
 
-    input = fopen("input.txt", "r");
+    input = fopen("input.txt", "r"); 
     while (!feof(input)) {
         fscanf(input, "%c", &a);
-        if (a == '1' || a == '0') {
+        if (a == '1' || a == '0') { 
             s[i][j] = a;
             j++;
         }
-        if (a == '\n') {
+        if (a == '\n') { 
             i++;
             j = 0;
         }
     }
-    for (int n = 0; n < j; n++) {
+    for (int n = 0; n < j; n++) { 
         count = 0;
         for (int m = 0; m <= i; m++) {
             if (s[m][n] == '1' && count == 0) {
                 count++;
-                b = m + 1;
+                b = m;
             }
             else if (s[m][n] == '1') {
                 count++;
-                if (count == 2) {
-                    printf("%d: %d\n", b, m + 1);
+                if (count == 2) { 
+                    s2[b][m] = '1';
+                    s2[m][b] = '1';
                 }
             }
         }
-        if (count == 1) {
-            printf("%d: %d\n", b, b);
+    }
+    for (int m = 0; m <= i; m++) {
+        printf("%d: ", m + 1);
+        count = 0;
+        for (int n = 0; n < j; n++) {
+            if (s2[m][n] == '1') {
+                printf("%d ", n + 1);
+                count++;
+            }
         }
+        printf("\n");
     }
     
-    output = fopen("output.dot", "w");
+    output = fopen("output.dot", "w"); 
     fprintf(output, "digraph Grah {\n");
     for (int n = 0; n < j; n++) {
         count = 0;
@@ -58,14 +68,14 @@ int main()
             fprintf(output, "%d -> %d\n", b, b);
         }
     }
-    for (int m = 0; m <= i; m++) {
+    for (int m = 0; m <= i; m++) { 
         count = 0;
         for (int n = 0; n < j; n++) {
             if (s[m][n] == '1') {
                 count++;
             }
         }
-        if (count == 0) {
+        if (count == 0) { 
             fprintf(output, "%d\n", m + 1);
         }
     }
